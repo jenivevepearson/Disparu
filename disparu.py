27afbc8f895feebe615e02d6bf4ae1e7d62592f6
Noah Franz <noahfranz13@gmail.com>
2025-05-09 17:04:35 -0700

    fix the bugs with not persisting on the updates

--- disparu.py
+++ disparu.py
@@ -6,6 +6,8 @@ from functools import partial
 from nicegui import app
 app.add_static_files('/images', 'static/images')
 
+CSV_PATH = "./annotations.csv"
+
 ################################
 # THE NICEGUI PAGE DEFINITIONS #
 ################################
@@ -26,58 +28,77 @@ async def galaxy_pages(galaxyname):
 
     with frame():
         ui.label(f"{galaxyname}").classes("text-h1")
-
+        
         img_paths = glob.glob(f"static/images/{galaxyname}/*")
         with ui.grid(columns=1).classes("w-full"):
             for img in img_paths:
-                existing = annotations[
-                    (annotations["galaxy"] == galaxyname) &
-                    (annotations["image"] == img)
-                ]
-                saved_follow_up = bool(existing["follow_up"].values[0]) if not existing.empty else False
-                saved_comment = str(existing["comment"].values[0]) if not existing.empty else ""
-
-                # Variables that store current values for this image
+                try:
+                    existing = annotations.loc[img]
+                    saved_follow_up = bool(existing["follow_up"])
+                    saved_comment = str(existing["comment"])  
+                except KeyError:
+                    # this is because the item doesn't exist in the df
+                    saved_follow_up = False
+                    saved_comment = ""
+                    
                 follow_up_value = saved_follow_up
                 comment_value = saved_comment
-
+                print(follow_up_value)
                 with ui.row().classes("w-full items-center justify-between"):
                     web_path = img.replace('static/images', '/images')
                     ui.image(web_path).classes("max-w-[80%] h-auto object-contain")
+                    img_path = ui.label(img).classes("text-h3")
                     with ui.column().classes("items-center"):
                         ui.label("Follow up?").classes("text-sm text-gray-600")
-                        check_update_func = partial(update_annotation, img_path=img, comment_input=comment_value, galaxyname=galaxyname)
-                        checkbox = ui.checkbox(value=follow_up_value, on_change=lambda e: check_update_func(value=e.sender.value))
+                        checkbox = ui.checkbox(
+                            value=follow_up_value,
+                            on_change=lambda e, img=img_path.text: update_check(img, e.sender, galaxyname)
+                        )
                         comment_input = ui.input(label='Comments', value=comment_value)
 
-
                         # Use partial to bind img, checkbox, comment_input values at definition time
-                        #checkbox.on("change", partial(update_annotation, img, checkbox, comment_input))
-                        comment_input.on("blur", partial(update_annotation, img, checkbox.value, comment_input, galaxyname))
+                        comment_input.on("blur", partial(update_comment, img, comment_input, galaxyname))
 
 
 ##################################
 # HELPER FUNCTIONS FOR THE PAGES #
 ##################################
-def update_annotation(img_path, value, comment_input, galaxyname):
+def update_check(img_path, check_value, galaxyname):
+    """
+    update the checkbox state in annotations
+    """
     global annotations
-    if type(comment_input) is str:
-        comment_value=comment_input
+    annotations = load_annotations()
+    if img_path in annotations.index:
+        annotations.loc[img_path, "follow_up"] = check_value.value
     else:
-        comment_value = comment_input.value
-    new_row = {
-                "galaxy": galaxyname,
-                "image": img_path,
-                "follow_up": value,
-                "comment": comment_value}
-    print(f"Updating annotation for {img_path}: {new_row}")
-    annotations = annotations[
-                    ~((annotations["galaxy"] == galaxyname) &
-                    (annotations["image"] == img_path))
-                    ]
-    annotations = pd.concat([annotations, pd.DataFrame([new_row])], ignore_index=True)
+        new_row = {
+            "galaxy": galaxyname,
+            "image": img_path,
+            "follow_up": check_value.value,
+            "comment": ""
+        }
+        annotations = pd.concat([annotations, pd.DataFrame([new_row])], ignore_index=True)
     save_annotations(annotations)
-
+    
+def update_comment(img_path, comment_input, galaxyname):
+    """
+    update the comment in annotations
+    """
+    global annotations
+    annotations = load_annotations()
+    if img_path in annotations.index:
+        annotations.loc[img_path, "comment"] = comment_input.value
+    else:
+        new_row = {
+            "galaxy": galaxyname,
+            "image": img_path,
+            "follow_up": False,
+            "comment": comment_input.value
+        }
+        annotations = pd.concat([annotations, pd.DataFrame([new_row])], ignore_index=True)
+    save_annotations(annotations)
+    
 def _post_table(galaxy_df):
 
     table = ui.table.from_pandas(galaxy_df).classes('width-500')
@@ -100,11 +121,11 @@ def _generate_table(images_dir):
 
     return pd.DataFrame(data)
 
-CSV_PATH = "./annotations.csv"
-
 def load_annotations():
     if os.path.exists(CSV_PATH):
-        return pd.read_csv(CSV_PATH)
+        annotations = pd.read_csv(CSV_PATH)
+        annotations.index = annotations.image
+        return annotations
     else:
         return pd.DataFrame(columns=["galaxy", "image", "follow_up", "comment"])
 
